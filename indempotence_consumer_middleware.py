d060853ab749bd745d39cc3859e0d4d035261c52
Tikhanovsky Grigory <Grisha-3108@yandex.ru>
2025-09-22 18:48:43 +0300

    Indempotence testing removed to middleware

---
+++ indempotence_consumer_middleware.py
@@ -0,0 +1,19 @@
+from typing import Any
+from hashlib import sha256
+
+from faststream import BaseMiddleware
+from faststream.broker.message import StreamMessage
+from faststream.exceptions import AckMessage
+
+from redis_connection import redis_client
+from config import settings
+
+class IndempotenceConsumer(BaseMiddleware):
+    async def consume_scope(self, call_next, msg: StreamMessage[Any]):
+        message_hash = sha256(msg.body).digest()
+        if key:=await redis_client.get(message_hash) != b'1':
+            res = await super().consume_scope(call_next, msg)
+            await redis_client.setex(message_hash, settings.kafka.duplicate_cache_time, b'1')
+        else:
+            raise AckMessage()
+        return res
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from hashlib import sha256
 from faststream import FastStream, Logger
 from faststream.kafka import KafkaBroker
 from faststream.kafka.annotations import KafkaMessage
+from faststream.asyncapi.schema import Contact
 from redis import ConnectionError
 
 from redis_connection import redis_client
@@ -10,27 +11,26 @@ from config import settings
 from logger_config import logger
 from schemas.email_message import EmailMessage
 from email_helper import send_message
+from indempotence_consumer_middleware import IndempotenceConsumer
 
+broker = KafkaBroker(f'{settings.kafka.host}:{settings.kafka.port}', 
+                     enable_idempotence=True,
+                     middlewares=[IndempotenceConsumer])
 
-broker = KafkaBroker(f'{settings.kafka.host}:{settings.kafka.port}')
 @broker.subscriber('send-email-message',
                    auto_commit=False,
                    group_id='email',
                    description='Сюда подаются сообщения для отправки через email',
                    title='input_data:Consume_email_message')
 async def send_email_consumer(body: EmailMessage, message: KafkaMessage, logger: Logger):
-    message_hash = sha256(message.body).digest()
-    if key:=await redis_client.get(message_hash) != b'1':
-        await send_message(body)
-        res = await redis_client.setex(message_hash,
-                                    settings.kafka.duplicate_cache_time,
-                                    b'1')
-        logger.debug('Ключ получен: %s, Ключ был добавлен: %s', key, res)
+    await send_message(body)
+
 
 app = FastStream(broker,
                  title='Pet проект по отправке email',
                  version='1.0.0',
                  description='Приложение, которое отправляет email, полученные от брокера',
+                 contact=Contact(name='Григорий Тихановский', email='Grisha-3108@yandex.ru')
                  )
 
 
